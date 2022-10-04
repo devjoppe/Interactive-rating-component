c27341a3d0bbbc5b1c310186384c1ff22eee67af
devjoppe <joakim.ottosson@medieinstitutet.se>
2022-10-04 14:30:31 +0200

    javascript submit functions

---
+++ js/submit.js
@@ -0,0 +1,23 @@
+let userChoice = 0;
+
+$("input:radio[name='valuefieldset']").click(function(){
+    userChoice = $(this).val();
+});
+
+$('#submitrating').click(function(){
+
+    console.log(userChoice);
+
+    // Content change
+    $('#description p').text('We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!');
+    $('#description p').addClass("center-text");
+    $('#title h1').text('Thank you!');
+    $('#title').addClass("center-text");
+
+    $('#topimages img').removeClass("star-icon");
+    $('#images').attr('src', 'images/illustration-thank-you.svg');
+    $('#topimages').addClass("center-text");
+
+    $('section').remove('#rating')
+    
+});
